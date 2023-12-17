72f337872797a21778894230a87392e25f152362
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-12-17 20:10:19 +0530

    Adding the seed value

--- 1-basic-client.py
+++ 1-basic-client.py
@@ -5,7 +5,8 @@ response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "user", "content":"Which language is best for C++ programming"}
-    ]
+    ],
+    seed=1,
 )
 
 print(response.choices[0].message.content)
