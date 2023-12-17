d691d24fc37be00ea96bd87309ba7126b3ea5181
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-12-17 18:37:38 +0530

    first basic chat completion GPT

---
+++ 1-basic-client.py
@@ -0,0 +1,11 @@
+import openai
+client = openai.OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content":"Which language is best for C++ programming"}
+    ]
+)
+
+print(response.choices[0].message.content)
